a7920dfa8725a104c8980304e5fd172c2afb3e05
trolle18 <92292475+trolle18@users.noreply.github.com>
2022-12-15 15:32:38 +0100

    --

--- components/NavBurgerMenu.js
+++ components/NavBurgerMenu.js
@@ -12,6 +12,9 @@ export default function NavBurgerMenu({data}) {
   const navOverflow = document.getElementById("nav-overflow");
   const body = document.body;
 
+  // const navLinkBtn = document.querySelectorAll("#nav-link-btn");
+  // const droplvl2 = document.getElementById("dropdown-lvl2");
+
   // Open burger menu
   const openMenu = () => {
     button.classList.toggle("change");
@@ -21,8 +24,7 @@ export default function NavBurgerMenu({data}) {
       dropdown.classList.remove("hidden");
       dropdown.classList.add("show");
       nav.classList.add("show")
-    } 
-    else {
+    } else {
       dropdown.classList.remove("show");
       dropdown.classList.add("hidden");
       nav.classList.remove("show")
@@ -33,8 +35,7 @@ export default function NavBurgerMenu({data}) {
       body.classList.add("overflow")
       navOverflow.classList.add("active")
       navOverflow.classList.remove("hidden")
-    } 
-    else {
+    } else {
       body.classList.remove("overflow")
       navOverflow.classList.remove("active")
       navOverflow.classList.add("hidden")
@@ -51,6 +52,18 @@ export default function NavBurgerMenu({data}) {
       }
     }));
   }
+
+  // const openLvl2 = () => {
+  //   navLinkBtn.classList.toggle("change")
+
+  //   if (droplvl2.classList.contains("hidden")) {
+  //     droplvl2.classList.remove("hidden")
+  //     droplvl2.classList.add("show")
+  //   } else {
+  //     droplvl2.classList.remove("show")
+  //     droplvl2.classList.add("hidden")
+  //   }
+  // }
   
 
   return (
@@ -64,14 +77,15 @@ export default function NavBurgerMenu({data}) {
 
       <div className="nav-mob-links__dropdown hidden" id="dropdown">
         <div className="nav-mob-links__dropdown__links-cntr">
-          {data?.navLinks.map((navLink) => ( 
-            <NavLink key={navLink.id} href={navLink.url} className="nav-mob-links__dropdown__links-cntr__link nav-link">
+          {data?.navLinks.map((navLink) => (
+            <NavLink key={navLink.id} href={navLink.url} className="nav-mob-links__dropdown__links-cntr__link nav-link nav-link-btn" id="nav-link-btn">
               <span className="nav-mob-links__dropdown__links-cntr__link__link-text">
                 <span>{navLink.text}</span>              
               </span>
             </NavLink>
           ))}  
         </div>
+
         <div className="nav-mob-links__dropdown__sec-links-cntr">
           {data?.subLinks.map((link) => ( 
             <NavLink key={link.id} href={link.url} className="nav-mob-links__dropdown__sec-links-cntr__link nav-link">
@@ -80,10 +94,22 @@ export default function NavBurgerMenu({data}) {
               </span>
             </NavLink>
           ))}  
-        </div>
-            
+        </div>            
         <SearchField/>
       </div>
+      
+      <div className="dropdown-lvl2 hidden" id="dropdown-lvl2">
+        <div className="dropdown-lvl2__links-cntr">
+          {data?.navLinks[0].subLvlLinks.map((link) => ( 
+            <NavLink key={link.id} href={link.url} className="dropdown-lvl2__links-cntr__link nav-link">
+              <span className="dropdown-lvl2__links-cntr__link__link-text">
+                <span>{link.text}</span>              
+              </span>
+            </NavLink>
+          ))}  
+        </div>        
+      </div>
+
     </>
   );
 };
