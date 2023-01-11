9f63a5ef41e7fb96dff7a1162e2cc22a4e79de70
Sofie Trolle <92292475+trolle18@users.noreply.github.com>
2023-01-11 09:30:19 +0100

    --

--- components/Hero.js
+++ components/Hero.js
@@ -14,7 +14,7 @@ export default function Hero({ data }) {
                 <div className="hero-cntr__video-cntr">                   
                      {data.video.map((video) => (
                         <video key={video.id}
-                        // autoPlay
+                        autoPlay
                         muted
                         >
                             <source src={video.src}/>
